eee17dcc48d80695bb84a0309cb5d97df9ed1d13
paulofachini <paulofachini@gmail.com>
2017-06-05 23:29:03 -0300

    Arquivo inicial para validar as configurações do Selenium, NUnit e ChromeDriver.

--- src/Selenium/Selenium/TestClass.cs
+++ src/Selenium/Selenium/TestClass.cs
@@ -1,15 +1,34 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Selenium
 {
     [TestFixture]
     public class TestClass
     {
-        [Test]
-        public void TestMethod()
+        static IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void SetUp()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            _driver = new ChromeDriver();
+            _driver.Url = "https://paulofachini.github.io/";
+        }
+
+        [Test]
+        public void TestCase()
+        {
+            IWebElement helloworld = _driver.FindElement(By.Id("helloworld"));
+            bool achou = helloworld.Text.Contains("Hello World");
+            Assert.IsTrue(achou);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+            _driver.Quit();
         }
     }
 }
