1efcc2a7b21fb51dc0ebbb3e61793ff81941aebd
Paulo Luiz Fachini <paulofachini@gmail.com>
2017-11-28 01:34:49 -0200

    Configurando 1º teste com BDD e Specflow.

--- src/Selenium/Selenium/TestClass.cs
+++ src/Selenium/Selenium/TestClass.cs
@@ -1,33 +1,33 @@
-﻿using NUnit.Framework;
-using OpenQA.Selenium;
+﻿//using NUnit.Framework;
+//using OpenQA.Selenium;
 
-namespace Selenium
-{
-    [TestFixture]
-    public class TestClass
-    {
-        private IWebDriver _driver;
+//namespace Selenium
+//{
+//    [TestFixture]
+//    public class TestClass
+//    {
+//        private IWebDriver _driver;
 
-        [OneTimeSetUp]
-        public void SetUp()
-        {
-            BrowserFactory.StartDriver("Chrome");
-            BrowserFactory.OpenUrl("https://paulofachini.github.io/");
-            _driver = BrowserFactory.Driver;
-        }
+//        [OneTimeSetUp]
+//        public void SetUp()
+//        {
+//            BrowserFactory.StartDriver("Chrome");
+//            BrowserFactory.OpenUrl("https://paulofachini.github.io/");
+//            _driver = BrowserFactory.Driver;
+//        }
 
-        [Test]
-        public void TestCase()
-        {
-            IWebElement helloworld = _driver.FindElement(By.Id("helloworld"));
-            bool achou = helloworld.Text.Contains("Hello World");
-            Assert.IsTrue(achou);
-        }
+//        [Test]
+//        public void TestCase()
+//        {
+//            IWebElement helloworld = _driver.FindElement(By.Id("helloworld"));
+//            bool achou = helloworld.Text.Contains("Hello World");
+//            Assert.IsTrue(achou);
+//        }
 
-        [OneTimeTearDown]
-        public void TearDown()
-        {
-            BrowserFactory.CloseDriver();
-        }
-    }
-}
+//        [OneTimeTearDown]
+//        public void TearDown()
+//        {
+//            BrowserFactory.CloseDriver();
+//        }
+//    }
+//}
